689aadea7c103626efe480d255b45f3105651f1c
Patryk Bakłaj <patrykbaklaj@gmail.com>
2017-08-07 22:48:04 +0200

    Bootstrap classes added

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ var campgrounds = [
   {name: "Salmon Creek", image: "https://img.hipcamp.com/image/upload/c_limit,f_auto,h_1200,q_60,w_1920/v1440478008/campground-photos/csnhvxn0qcki2id5vxnc.jpg"},
   {name: "Granite Hill", image: "http://soaringeaglecampground.com/test/wp-content/uploads/2014/01/view-of-the-basket-at-Soaring-Eagle.jpg"},
   {name: "Montain Goat", image: "https://www.nhstateparks.org/uploads/images/Dry-River_Campground_02.jpg"},
+  {name: "Deep hole", image: "http://pickwick-dam.com/wp-content/uploads/2015/08/17991101764_fcb19c7311_k.jpg"},
+  {name: "Salmon Creek", image: "https://img.hipcamp.com/image/upload/c_limit,f_auto,h_1200,q_60,w_1920/v1440478008/campground-photos/csnhvxn0qcki2id5vxnc.jpg"},
+  {name: "Granite Hill", image: "http://soaringeaglecampground.com/test/wp-content/uploads/2014/01/view-of-the-basket-at-Soaring-Eagle.jpg"},
+  {name: "Montain Goat", image: "https://www.nhstateparks.org/uploads/images/Dry-River_Campground_02.jpg"},
   {name: "Deep hole", image: "http://pickwick-dam.com/wp-content/uploads/2015/08/17991101764_fcb19c7311_k.jpg"}
 ];
 

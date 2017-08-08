5b883cd0315a4972566c0693b8014ce62e5ed32c
Patryk Bakłaj <patrykbaklaj@gmail.com>
2017-08-08 20:37:37 +0200

    MongoDB connected and added to app.js

--- app.js
+++ app.js
@@ -1,56 +1,89 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express     = require("express"),
+app         = express(),
+bodyParser  = require("body-parser"),
+mongoose    = require("mongoose");
 
+// connecting to the database
+mongoose.connect("mongodb://localhost/yelp_camp");
+
+// app setup
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-
-var campgrounds = [
-  {name: "Salmon Creek", image: "https://img.hipcamp.com/image/upload/c_limit,f_auto,h_1200,q_60,w_1920/v1440478008/campground-photos/csnhvxn0qcki2id5vxnc.jpg"},
-  {name: "Granite Hill", image: "http://soaringeaglecampground.com/test/wp-content/uploads/2014/01/view-of-the-basket-at-Soaring-Eagle.jpg"},
-  {name: "Montain Goat", image: "https://www.nhstateparks.org/uploads/images/Dry-River_Campground_02.jpg"},
-  {name: "Deep hole", image: "http://pickwick-dam.com/wp-content/uploads/2015/08/17991101764_fcb19c7311_k.jpg"},
-  {name: "Salmon Creek", image: "https://img.hipcamp.com/image/upload/c_limit,f_auto,h_1200,q_60,w_1920/v1440478008/campground-photos/csnhvxn0qcki2id5vxnc.jpg"},
-  {name: "Granite Hill", image: "http://soaringeaglecampground.com/test/wp-content/uploads/2014/01/view-of-the-basket-at-Soaring-Eagle.jpg"},
-  {name: "Montain Goat", image: "https://www.nhstateparks.org/uploads/images/Dry-River_Campground_02.jpg"},
-  {name: "Deep hole", image: "http://pickwick-dam.com/wp-content/uploads/2015/08/17991101764_fcb19c7311_k.jpg"}
-];
-
-
-app.get("/", function(req, res){
-  res.render("landing");
+// schema setup
+var campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String
 });
 
-app.get("/campgrounds", function(req, res){
-  res.render("campgrounds", {campgrounds: campgrounds});
-});
+// compiling into a model
+var Campground = mongoose.model("Campground", campgroundSchema);
 
-app.post("/campgrounds", function(req, res){
-  // get data from form
-  var name = req.body.name;
-  var image = req.body.image;
-  var newCampground = {
-    name: name,
-    image: image
-  }
-  // add data to array
-  campgrounds.push(newCampground);
-  // redirect
-  res.redirect("/campgrounds");
-});
-
-app.get("/campgrounds/new", function(req, res){
-  res.render("new.ejs");
-});
-
-app.get("/*", function(req, res){
-  res.send("Sorry! Page not found! Please make sure the URL is correct");
-});
+// creating sample item and adding to the database
+// Campground.create(
+//   {
+//     name: "Granite Hill",
+//     image: "http://soaringeaglecampground.com/test/wp-content/uploads/2014/01/view-of-the-basket-at-Soaring-Eagle.jpg"}, function(err, item) {
+//       if (err) {
+//         console.log("An Error Occurred");
+//         console.log(err);
+//       } else {
+//         console.log("successfully created and added item to database");
+//         console.log(item);
+//       }
+//     });
 
 
+    app.get("/", function(req, res){
+      res.render("landing");
+    });
 
-app.listen(3000, function(){
-  console.log("Server is listening on port 3000");
-  console.log("YelpCamp v1");
-});
+    app.get("/campgrounds", function(req, res){
+      // get All camps from database
+      Campground.find({}, function(err, allCamps){
+        if (err) {
+          console.log("An error occurred");
+          console.log(err);
+        } else {
+          console.log("successfully added new item");
+          console.log(allCamps);
+          res.render("campgrounds", {campgrounds: allCamps});
+        }
+      });
+    });
+
+    app.post("/campgrounds", function(req, res){
+      // get data from form
+      var name = req.body.name;
+      var image = req.body.image;
+      var newCampground = {
+        name: name,
+        image: image
+      }
+      // creating new camp and save to the database
+      Campground.create(newCampground, function(err, newCamp){
+        if (err) {
+          console.log("An error occurred");
+          console.log(err);
+        } else {
+          console.log("successfully added new item to DB");
+          // redirecting to campgrounds
+          res.redirect("/campgrounds");
+        }
+      });
+    });
+
+    app.get("/campgrounds/new", function(req, res){
+      res.render("new.ejs");
+    });
+
+    app.get("/*", function(req, res){
+      res.send("Sorry! Page not found! Please make sure the URL is correct");
+    });
+
+
+
+    app.listen(3000, function(){
+      console.log("Server is listening on port 3000");
+      console.log("YelpCamp v1");
+    });

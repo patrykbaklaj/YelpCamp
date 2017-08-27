835ba870a260381c162987ae2679f6f5b8635687
Patryk Bakłaj <patrykbaklaj@gmail.com>
2017-08-27 20:40:37 +0200

    Association User with Campground made

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -16,15 +16,20 @@ router.get("/", function(req, res){
 });
 
 // CREATE - add new camps to database
-router.post("/", function(req, res){
+router.post("/", isLoggedIn , function(req, res){
   // get data from form
   var name = req.body.name;
   var image = req.body.image;
   var desc = req.body.description;
+  var author = {
+    id: req.user._id,
+    username: req.user.username
+  }
   var newCampground = {
     name: name,
     image: image,
-    description: desc
+    description: desc,
+    author: author
   }
   // creating new camp and save to the database
   Campground.create(newCampground, function(err, newCamp){
@@ -39,7 +44,7 @@ router.post("/", function(req, res){
 });
 
 // NEW - display form to add new camp
-router.get("/new", function(req, res){
+router.get("/new", isLoggedIn , function(req, res){
   res.render("campgrounds/new");
 });
 
@@ -55,5 +60,12 @@ router.get("/:id", function(req, res){
     }
   });
 });
+
+function isLoggedIn(req, res, next){
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
+}
 
 module.exports = router;
